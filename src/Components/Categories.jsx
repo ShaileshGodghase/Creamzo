6d4a0f12a1372385397275f42d388e69e1a1335d
Shailesh Godghase <shaileshgodghase19@gmail.com>
2022-01-10 17:07:47 +0530

    Some changes

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import '../Css/Navbar.css'
 import { Link } from 'react-router-dom';
 
@@ -11,15 +11,15 @@ function Categories() {
             <div className="all-category">
                 <div className="cat">
                     <p><Link to="/">Trending</Link></p>
-                    <p><Link to="/world">World</Link></p>
-                    <p><Link to="/business">Business</Link></p>
-                    <p><Link to="/technology">Technology</Link></p>
-                    <p><Link to="/entertainment">Entertainment</Link></p>
-                    <p><Link to="/sports">Sports</Link></p>
-                    <p><Link to="/science">Science</Link></p>
-                    <p><Link to="/health">Health</Link></p>
-                    <p><Link to="/fashion">Fashion</Link></p>
-                    <p><Link to="/literature">Literature</Link></p>
+                    <p><Link to="world">World</Link></p>
+                    <p><Link to="business">Business</Link></p>
+                    <p><Link to="technology">Technology</Link></p>
+                    <p><Link to="entertainment">Entertainment</Link></p>
+                    <p><Link to="sports">Sports</Link></p>
+                    <p><Link to="science">Science</Link></p>
+                    <p><Link to="health">Health</Link></p>
+                    <p><Link to="fashion">Fashion</Link></p>
+                    <p><Link to="literature">Literature</Link></p>
                 </div>
             </div>
          </div>
